73a616534efa193dac32f632eaa4668d37a80652
gopaljoshi <gopaljoshi@Gopal>
2016-08-03 09:34:12 +1200

    added one object in MainInformation class

---
+++ Library Management System/src/MainInformation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MainInformation
+{
+	private static Scanner scanner;
+	public static void main(String[]args)
+	{
+	 /*Allow user to input the Book details */
+
+		System.out.println("Enter the ISBN : ");
+		scanner =new Scanner(System.in);
+		String strISBN=scanner.next();
+		System.out.println("Enter the Book title : ");
+		String strBook=scanner.next();
+		System.out.println("Enter the Author Name : ");
+		String strAuthor=scanner.next();
+		System.out.println("Enter the Book Edition : ");
+		String strEdition=scanner.next();
+	
+	 /*Call BookDisplay and pass the user input to display using DisplayBookInfo() */
+		
+		BookDisplay BD1 = new BookDisplay(strISBN, strBook, strAuthor,strEdition);
+		BD1.DisplayBookInfo();
+	}
+}
